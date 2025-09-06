105a0a8d57cc1c528a8efc77baab74de32371395
Pramod Kumar Yadav <pramodyadav027@gmail.com>
2025-09-06 22:26:43 +0200

    add junit platform launcher dependency to get capabilities to run tasks at global test run level

---
+++ src/main/java/com/powertester/extensionsglobal/GlobalSuiteListener.java
@@ -0,0 +1,54 @@
+package com.powertester.extensionsglobal;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.platform.launcher.LauncherSession;
+import org.junit.platform.launcher.LauncherSessionListener;
+
+import com.powertester.database.DBConnection;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GlobalSuiteListener implements LauncherSessionListener {
+  private long testRunStartTime;
+  private static final Path TEST_REPORT_PATH = Paths.get(".", "test-reports");
+
+  /**
+   * This method is called when the launcher session is opened, i.e., at the very start of the entire test run.
+   * It is used to perform global setup tasks such as initializing resources that are shared across all tests.
+   * Examples include starting containers, seeding databases, or initializing global fixtures.
+   */
+  @Override
+  public void launcherSessionOpened(LauncherSession session) {
+    log.info("🚀 Test run started.");
+
+    testRunStartTime = System.currentTimeMillis();
+
+    // Initialize DB connection pool
+    DBConnection.getInstance();
+
+    // Create test report directory if it doesn't exist
+    try {
+      Files.createDirectories(TEST_REPORT_PATH);
+    } catch (IOException e) {
+      log.error("Error creating test report directory", e);
+    }
+  }
+
+  /**
+   * This method is called when the launcher session is closed, i.e., at the very end of the entire test run.
+   * It is used to perform global teardown tasks such as cleaning up resources that were shared across all tests.
+   * Examples include stopping containers, closing database connections, or exporting test reports.
+   */
+  @Override
+  public void launcherSessionClosed(LauncherSession session) {
+    log.info("✅ Closing Hikari datasource pool (only once) at the end of the whole test run"); 
+    DBConnection.getInstance().closeConnectionPool();
+
+    log.info("⌛️ Test run completed in {} seconds.", (System.currentTimeMillis() - testRunStartTime) / 1000.0);
+  }
+}
